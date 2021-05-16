5c827ae71bc1beeded2b28e6e4d4eb099532ac76
Piotr Gronowski <peter80gro@gmail.com>
2021-05-16 03:47:51 +0200

    Hydrapp v.1.0.0

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,41 @@
 import '../scss/main.scss';
 
 // uncomment the lines below to enable PWA
-// import {registerSW} from './pwa.js';
-// registerSW();
+ import {registerSW} from './pwa.js';
+ registerSW();
 
 /* place your code below */
 
 console.log('HELLO 🚀')
+
+const glassCounter = document.querySelector('.glass_counter--js');
+const buttonAdd = document.querySelector('.button-add--js');
+const buttonRemove = document.querySelector('.button-remove--js');
+const key = new Date().toISOString().slice(0,10);
+
+let currentGlassCounter = 0;
+
+const localStorageValue = localStorage.getItem(key);
+
+if (localStorageValue){
+    currentGlassCounter = localStorageValue;
+}else{
+    localStorage.setItem(key,0);
+}
+
+glassCounter.innerHTML = currentGlassCounter;
+
+buttonAdd.addEventListener('click',()=>{
+    currentGlassCounter++;
+    glassCounter.innerHTML = currentGlassCounter;
+    localStorage.setItem(key, currentGlassCounter);
+})
+buttonRemove.addEventListener('click',()=>{
+    if(currentGlassCounter>0){
+        currentGlassCounter--;
+        glassCounter.innerHTML = currentGlassCounter;
+        localStorage.setItem(key, currentGlassCounter);
+    }else{
+        alert('Z pustego i Salomon nie naleje')
+    }
+})
